aa10598ec66868396c0a2f4cf51c4b1cb42ca8ad
dave <you@example.com>
2024-02-02 13:58:19 +0000

    Wk2 Update

--- bank.py
+++ bank.py
@@ -1,12 +1,16 @@
 # Week2 Task - Converting two different cent values into one euro amount
 
-# Need to convert input into an integer 
+# Creating two variables & converting input into an integer.
+# Without converting we can't perform mathematical function
 
 amount_one = int(input("Enter amount in cents "))
 amount_two = int(input("Enter second amount in cents "))
 
 total_amount = amount_one + amount_two
+
 #Convert to euros
+
 euro_value = total_amount / 100
+
 # Can only concatenate str (not "float") to str
 print("€" + str(euro_value))
