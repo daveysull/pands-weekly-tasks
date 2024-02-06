2269a2fd9f3a13b49ccffbd7499ee4610bc7230d
dave <you@example.com>
2024-02-06 17:52:07 +0000

    Week 3

---
+++ accounts.py
@@ -0,0 +1,6 @@
+# Week3 Task - Aim is to input a 10 digit number and for the output to be the last 4 digits and *'s
+
+account_number = input("Please enter an 10 digit account number")
+
+print("Your account number is  XXXXXX" + str(account_number[-4:]))
+
