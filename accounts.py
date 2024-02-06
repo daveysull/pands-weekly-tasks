7e9910c9fe70abb5d1de9e0ba23cbeb2c2a35040
dave <you@example.com>
2024-02-06 17:52:38 +0000

    Week 3

--- accounts.py
+++ accounts.py
@@ -1,4 +1,4 @@
-# Week3 Task - Aim is to input a 10 digit number and for the output to be the last 4 digits and *'s
+# Week3 Task - Aim is to input a 10 digit number and for the output to be the last 4 digits and X's
 
 account_number = input("Please enter an 10 digit account number")
 
